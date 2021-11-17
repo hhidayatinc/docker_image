cad1241c400c87c62f8130c4ca49d1abd05df423
hhidayatinc <chasanah55@gmail.com>
2021-11-17 16:24:48 +0700

    first commit

---
+++ Latihan.java
@@ -0,0 +1,11 @@
+public class Latihan {
+	public static void main(String[] args) {
+		System.out.println("Hello Dasar Pemrograman");
+
+			System.out.println("----------------------------------------");
+			System.out.println("Biodata Mahasiswa Baru Polinema			");
+			System.out.println("----------------------------------------");
+			System.out.println("NIM    : 1931710028		       			");
+			System.out.println("Nama   : Hidayati Nur Chasanah		    ");
+	}
+}
